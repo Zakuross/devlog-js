edeeff8010f7ba86671901bf40b14b5b184648de
[Zakariya] <[zakariya.belmokhtar@le-campus-numerique.fr]>
2023-05-23 14:28:52 +0200

    exo1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,9 @@
 // you can write js here
-console.log('hello from file');
+console.log('hello from file'); 
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+let celsius = kelvins - 273;
+
+let fahrenheits = celsius * (9/5) + 32;
+
+console.log(Math.floor(fahrenheits));
