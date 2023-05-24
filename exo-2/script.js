9be29be18f4a0af938507f2a808a07df141ed684
[Zakariya] <[zakariya.belmokhtar@le-campus-numerique.fr]>
2023-05-24 09:14:09 +0200

    exo2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,33 @@
 // you can write js here
 console.log('exo-2');
+
+const myDate = new Date('May 22, 2023 09:00')
+
+console.log(myDate.getDay())
+let semaine = "c'est la semaine"
+let weekend = "c'est le weekend"
+let heure = myDate.getHours()
+console.log(heure)
+//if(myDate.getDay() > 0 && myDate.getDay() < 6){
+    //console.log(semaine)
+
+//} else {
+    //console.log(weekend)
+//}
+function isWeek(myDateParameter){
+    return myDateParameter.getDay() > 0 && myDateParameter.getDay() < 6
+}
+function FridayWeekend(date){
+    return date.getDay() == 5 && heure >16
+}
+
+if(isWeek(myDate)&&(FridayWeekend(myDate)==false)&&(MondayWeek(myDate)==false)){
+    console.log(semaine)
+
+} else {
+    console.log(weekend)
+}
+
+function MondayWeek(date2){
+    return date2.getDay() == 1 && heure < 9
+}
