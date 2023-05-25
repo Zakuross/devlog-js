21f389f2f5c2b863790cbd74bae260a418a4fdf5
[Zakariya] <[zakariya.belmokhtar@le-campus-numerique.fr]>
2023-05-25 11:43:35 +0200

    exo4

--- exo-4/script.js
+++ exo-4/script.js
@@ -2,4 +2,22 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];  
+
+let last = secretMessage.pop()
+
+let newL = secretMessage.push('to')
+
+let newL2 = secretMessage.push('program')
+
+let first = secretMessage.shift()
+
+let six = secretMessage[6] = 'right'
+
+let newL3 = secretMessage.unshift('Programming')
+
+let four = secretMessage.splice(5, 5, 'know')
+
+
+
+console.log(secretMessage.join())
